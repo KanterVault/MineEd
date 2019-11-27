0b99f00a1f5d341e2ce491c08f6c9aa945645ceb
Vadimchik79 <vadim.komarov.2000.mm@mail.ru>
2019-11-27 05:00:19 +0500

    Изменение в Form Designer.

--- Project/WindowsFormsApp1/Form1.Designer.cs
+++ Project/WindowsFormsApp1/Form1.Designer.cs
@@ -42,7 +42,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(624, 442);
+            this.ClientSize = new System.Drawing.Size(532, 369);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MinimumSize = new System.Drawing.Size(256, 128);
             this.Name = "Form1";
